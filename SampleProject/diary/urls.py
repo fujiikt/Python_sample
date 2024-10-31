dbf56a7238d4382196c2d5bcf89c24f7c514ea13
kota <kotafuji0720@gmail.com>
2024-10-31 11:21:29 +0900

    create_update

--- SampleProject/diary/urls.py
+++ SampleProject/diary/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path("page/create/", views.page_create, name="page_create"),
   path("pages/", views.page_list, name="page_list"),
   path("page/<uuid:id>/", views.page_detail, name="page_detail"),
+  path("page/<uuid:id>/update", views.page_update, name="page_update"),
 ]
