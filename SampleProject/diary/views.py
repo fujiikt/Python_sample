45ed1fe751eef436a5cca3a2f87445d1349812be
kota <kotafuji0720@gmail.com>
2024-10-31 10:14:43 +0900

    create_list_page

--- SampleProject/diary/views.py
+++ SampleProject/diary/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from .forms import PageForm
+from .models import Page
 
 class IndexView(View):
     def get(self, request):
@@ -25,5 +26,12 @@ class PageCreationView(View):
             return redirect("diary:index")
         return render(request, "diary/page_form.html", {"form": form})
 
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.order_by("-page_date")
+        return render(request, "diary/page_list.html", {"page_list": page_list})
+
+
 index = IndexView.as_view()
 page_create = PageCreationView.as_view()
+page_list = PageListView.as_view()
